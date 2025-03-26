645d2ab22a87c4b4e7b71dd2fc264fb0d794ec95
GAmer1241Jerry <Teakettlekeaton@gmail.com>
2025-03-26 13:03:24 -0700

    Updated more test code

--- Main/MainAppFolder/main.py
+++ Main/MainAppFolder/main.py
@@ -1,6 +1,42 @@
 import flet as ft
+import vlc
+import todo
+from tinytag import TinyTag
+from tinytag import Image as TTimage
+from PIL import Image
+import io
 
-def main(page: ft.Page):
-    page.add(ft.Text(value="Hello, world!"))
 
+def add_clicked(e):
+    todo.MusicPlay()
+
+def main(page):
+    page.title = "Card Example"
+    page.add(
+        ft.Card(
+
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.ListTile(
+                            leading=ft.Icon(ft.Icons.ALBUM),
+                            title=ft.Text("The Enchanted Nightingale"),
+                            subtitle=ft.Text(
+                                "Music by Julie Gable. Lyrics by Sidney Stein."
+                            ),
+                        ),
+                        ft.Row(
+                            [ft.TextButton("Buy tickets", on_click=add_clicked ), ft.TextButton("Listen")],
+                            alignment=ft.MainAxisAlignment.END,
+
+                        ),
+                    ]
+
+                ),
+
+        )
+    )
+)
+
+    page.add
 ft.app(main)
--- Main/MainAppFolder/todo.py
+++ Main/MainAppFolder/todo.py
@@ -2,3 +2,8 @@ import flet as ft
 from tinytag import TinyTag
 # importing vlc module
 import vlc
+
+
+def MusicPlay():
+    media = vlc.MediaPlayer("./Music/MCLS.mp3")
+    media.play()
